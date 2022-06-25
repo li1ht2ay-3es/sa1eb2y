7ff8d7520176e8c9387adda65764d05594e2259f
li1ht2ay <li1ht2ay@users.noreply.github.com>
2022-06-25 05:29:07 +0000

    [libretro] integrate new hacks

---
+++ libretro/libretro-options.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+#include <Core/gb.h>
+#include "libretro.h"
+#include "libretro-options.h"
+
+
+#include "crop-border.c"
+#include "fast-bootrom.c"
+#include "game-lag.c"
+#include "osd-message.c"
+#include "sprite-limit.c"
+#include "sprite-priority.c"
+#include "video-overclock.c"
+#include "volume-level.c"
+#include "vram-blocking.c"
